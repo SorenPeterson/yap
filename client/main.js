b23355f1a2ef2e14a518217afdb4f0c43072ace2
Soren Peterson <soren468@gmail.com>
2015-10-14 22:43:49 -0700

    links in nav and add a pokemon button

--- client/main.js
+++ client/main.js
@@ -2,3 +2,22 @@ window.Pokemon = new Mongo.Collection("pokemon");
 
 Meteor.subscribe("pokemon");
 
+var Team = function() {
+}
+
+Team.prototype.full = function() {
+	return false;
+}
+
+var team;
+
+Template.TeamBuilder.onCreated(function() {
+	team = new Team();
+});
+
+Template.TeamBuilder.helpers({
+	team_is_full: function() {
+		return team.full();
+	}
+});
+
